26e3cbf3e968378a73556ce24d2f426c998dab1f
olgapgit <olga.prianikova@gmail.com>
2015-01-31 19:49:11 +0200

    comments

--- Sort Array/src/com/company/Main.java
+++ Sort Array/src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
         //int[] myArray = {7, 1, 17, 50,9,5,11};
         int[] myArray = {1, 25, 17, 50,9,5,51,90};
@@ -13,12 +12,12 @@ public class Main {
                 + arr[4] + ", " + arr[5] + ", " + arr[6]+", "+arr[7]);
         for (int i = 0; i < arr.length - 1; i++) {
             int min = arr[i];
-            if (min > arr[i + 1]) {
+            if (min > arr[i + 1]) { // flip two numbers: [i], [i+1] and get the minimum
                 min = arr[i + 1];
                 arr[i + 1] = arr[i];
                 arr[i] = min;
             }
-            while (i>0 && min <= arr[i - 1]) {
+            while (i>0 && min <= arr[i - 1]) { // while minimum smaller than previous number: [i-1] - flip previous and minimum
                 arr[i] = arr[i - 1];
                 arr[i - 1] = min;
                 i--;
